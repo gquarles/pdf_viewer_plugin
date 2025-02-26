f9c1621813ca6a97366f5e11a5d0f24f5bd0c3d3
Griffin Quarles <griffinq178@gmail.com>
2025-02-26 12:21:22 -0500

    WIP android migration

--- android/src/main/java/dev/britto/pdf_viewer_plugin/PdfViewerPlugin.java
+++ android/src/main/java/dev/britto/pdf_viewer_plugin/PdfViewerPlugin.java
@@ -3,46 +3,33 @@ package dev.britto.pdf_viewer_plugin;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.BinaryMessenger;
 
+/**
+ * PdfViewerPlugin using the new Flutter v2 plugin embedding.
+ * 
+ * <p>If you need backward compatibility with older Flutter versions (v1 embedding),
+ * you should keep or restore the static registerWith(...) method referencing
+ * io.flutter.plugin.common.PluginRegistry.Registrar. However, that method will not
+ * compile in newer Flutter environments where Registrar no longer exists.
+ */
 public class PdfViewerPlugin implements FlutterPlugin {
-  /**
-   * Add an instance of this to {@link io.flutter.embedding.engine.plugins.PluginRegistry} to
-   * register it.
-   *
-   * <p>THIS PLUGIN CODE PATH DEPENDS ON A NEWER VERSION OF FLUTTER THAN THE ONE DEFINED IN THE
-   * PUBSPEC.YAML. Text input will fail on some Android devices unless this is used with at least
-   * flutter/flutter@1d4d63ace1f801a022ea9ec737bf8c15395588b9. Use the V1 embedding with  to use this plugin with older Flutter versions.
-   *
-   * <p>Registration should eventually be handled automatically by v2 of the
-   * GeneratedPluginRegistrant. https://github.com/flutter/flutter/issues/42694
-   */
-  public PdfViewerPlugin() {}
 
-  /**
-   * Registers a plugin implementation that uses the stable {@code io.flutter.plugin.common}
-   * package.
-   *
-   * <p>Calling this automatically initializes the plugin. However plugins initialized this way
-   * won't react to changes in activity or context, unlike CameraPlugin.
-   */
-  @SuppressWarnings("deprecation")
-  public static void registerWith(io.flutter.plugin.common.PluginRegistry.Registrar registrar) {
-    registrar
-            .platformViewRegistry()
-            .registerViewFactory(
-                    "pdf_viewer_plugin",
-                    new PdfViewerFactory(registrar.messenger(), registrar.view()));
+  public PdfViewerPlugin() {
+    // Default constructor
   }
 
   @Override
   public void onAttachedToEngine(FlutterPluginBinding binding) {
     BinaryMessenger messenger = binding.getBinaryMessenger();
     binding
-            .getPlatformViewRegistry()
-            .registerViewFactory(
-                    "pdf_viewer_plugin", new PdfViewerFactory(messenger, /*containerView=*/ null));
+        .getPlatformViewRegistry()
+        .registerViewFactory(
+            "pdf_viewer_plugin",
+            new PdfViewerFactory(messenger, /* containerView= */ null)
+        );
   }
 
   @Override
   public void onDetachedFromEngine(FlutterPluginBinding binding) {
+    // Clean up if necessary
   }
 }
